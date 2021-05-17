68e53a1a547bb56ffff7d8728331f20c787b8959
v1rtl <pilll.pl22@gmail.com>
2021-05-17 19:48:21 +0300

    add cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,39 @@
 import typescript from '@rollup/plugin-typescript'
 import copy from 'rollup-plugin-copy-assets'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
       dir: 'dist',
       format: 'esm'
-    }
-  ],
-  plugins: [
-    typescript({ include: ['./src/**/*.ts'] }),
-    copy({
-      assets: [
-        // You can include directories
-        'src/list.json'
-      ]
-    })
-  ]
-}
+    },
+    plugins: [
+      typescript({ include: ['./src/**/*.ts'] }),
+      copy({
+        assets: [
+          // You can include directories
+          'src/list.json'
+        ]
+      })
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs'
+      }
+    ],
+    plugins: [
+      typescript({ include: ['./src/**/*.ts'], plugins: [{ transform: '@magic-works/commonjs-import.meta' }] }),
+      copy({
+        assets: [
+          // You can include directories
+          'src/list.json'
+        ]
+      })
+    ]
+  }
+]
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { readFile } from 'fs/promises'
+import { readFileSync } from 'fs'
 import { shared, ERC20BalanceArgs } from './shared'
 
 const __dirname = new URL('.', import.meta.url).pathname
 
-const list = JSON.parse((await readFile(__dirname + '/list.json')).toString())
+const list = JSON.parse(readFileSync(__dirname + '/list.json').toString())
 
 export const ERC20Balance = (args: ERC20BalanceArgs) => shared(list)(args)
