1690b671506d31a742408bcbcf979dea8eceeac4
v1rtl <pilll.pl22@gmail.com>
2021-05-18 16:11:22 +0300

    add list export for node

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { readFileSync } from 'fs'
-import { shared, ERC20BalanceArgs } from './shared'
+import { shared, ERC20BalanceArgs, Token } from './shared'
 
 const __dirname = new URL('.', import.meta.url).pathname
 
-const list = JSON.parse(readFileSync(__dirname + '/list.json').toString())
+const list = JSON.parse(readFileSync(__dirname + '/list.json').toString()) as Token[]
+
+export { list }
 
 export const ERC20Balance = (args: ERC20BalanceArgs) => shared(list)(args)
