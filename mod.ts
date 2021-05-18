5dd4416573f6ad20e68690a27befd45f4e1b2bee
v1rtl <pilll.pl22@gmail.com>
2021-05-18 21:05:29 +0300

    fix double json parsing in deno

--- mod.ts
+++ mod.ts
@@ -1,10 +1,8 @@
 import { shared, ERC20BalanceArgs, Token } from './src/shared.ts'
 
-const list = JSON.parse(
-  await fetch(
-    'https://gitcdn.link/cdn/talentlessguy/erc20-balance/90f426aea0c94cfaf901fd0f002cfb00bad9d5f1/src/list.json'
-  ).then((res) => res.json())
-) as Token[]
+const list = (await fetch(
+  'https://gitcdn.link/cdn/talentlessguy/erc20-balance/90f426aea0c94cfaf901fd0f002cfb00bad9d5f1/src/list.json'
+).then((res) => res.json())) as Token[]
 
 export { list }
 
