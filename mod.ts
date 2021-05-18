90f426aea0c94cfaf901fd0f002cfb00bad9d5f1
v1rtl <pilll.pl22@gmail.com>
2021-05-18 20:56:00 +0300

    fix deno path

--- mod.ts
+++ mod.ts
@@ -1,6 +1,10 @@
 import { shared, ERC20BalanceArgs, Token } from './src/shared.ts'
 
-const list = JSON.parse(await Deno.readTextFile('./src/list.json')) as Token[]
+const list = JSON.parse(
+  await fetch('https://raw.githubusercontent.com/talentlessguy/erc20-balance/master/src/list.json').then((res) =>
+    res.json()
+  )
+) as Token[]
 
 export { list }
 
